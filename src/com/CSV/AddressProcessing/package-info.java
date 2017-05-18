392f50c6a840c19bc702d25ef55383ac7f1563a4
Nazar Ivanchuk <binary.root@gmail.com>
2017-05-18 17:08:01 +0300

    Remove old implementation

--- src/com/CSV/AddressProcessing/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Test task for Trainline company.
- * Application provides API for CSV parsing and writing. This package is deprecated.
- *
- * @since 1.0
- * @version 1.0
- * @author Nazar Ivanchuk
- */
-package com.CSV.AddressProcessing;
